d2ad1e25b9a97aeff44c25ecba7ac61ee804a427
Stef Ki <stevekiour@gmail.com>
2016-04-30 00:47:41 +0200

    Migrated to GitHub

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 under-construction-puzzle
+
+Old "under construction" page replacement, keeping for legacy reasons
---
+++ js/Object.identical.js
@@ -0,0 +1,26 @@
+/*
+    Original script title: "Object.identical.js"; version 1.12
+    Copyright (c) 2011, Chris O'Brien, prettycode.org
+    http://github.com/prettycode/Object.identical.js
+*/
+
+Object.identical = function (a, b, sortArrays) {
+        
+    function sort(object) {
+        if (sortArrays === true && Array.isArray(object)) {
+            return object.sort();
+        }
+        else if (typeof object !== "object" || object === null) {
+            return object;
+        }
+
+        return Object.keys(object).sort().map(function(key) {
+            return {
+                key: key,
+                value: sort(object[key])
+            };
+        });
+    }
+    
+    return JSON.stringify(sort(a)) === JSON.stringify(sort(b));
+};
